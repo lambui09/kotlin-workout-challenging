0825c6285c95786c5fcb13ad5f8ad40300bf3d22
Bui Duc Lam <buiduclam09@gmail.com>
2021-03-01 09:22:57 +0700

    handle test nals

---
+++ app/src/main/java/com/ktx/sample/views/nals/CityActivity.kt
@@ -0,0 +1,12 @@
+package com.ktx.sample.views.nals
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ktx.sample.R
+
+class CityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city)
+    }
+}
